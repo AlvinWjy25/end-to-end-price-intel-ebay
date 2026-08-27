b6d0b94e28f199e8927d8d800262c337bcf4f879
Alvin Wijaya <alvinwjy250@gmail.com>
2026-08-27 19:38:55 +0700

    updating evaluate.py to include random seed split metrics for tuned model

--- src/evaluate.py
+++ src/evaluate.py
@@ -4,12 +4,14 @@ import fastparquet
 import numpy as np
 import sys
 import os
+import joblib
 from pathlib import Path
 from datetime import datetime
 
 root_dir = Path(__file__).resolve().parent.parent
 sys.path.append(str(root_dir))
-from config.config_script import load_dataframe, setup_logger, X_TRAIN_PATH, X_TEST_PATH, Y_TRAIN_PATH, Y_TEST_PATH, Y_PRED_PATH, DF_META_PATH, EVAL_SUMMARY_REGRESSION_PATH, EVAL_DATAFRAME_REGRESSION_PATH
+from sklearn.model_selection import train_test_split
+from config.config_script import load_dataframe, setup_logger, X_TRAIN_PATH, X_TEST_PATH, Y_TRAIN_PATH, Y_TEST_PATH, Y_PRED_PATH, DF_META_PATH, EVAL_SUMMARY_REGRESSION_PATH, EVAL_DATAFRAME_REGRESSION_PATH, MODEL_REGRESSION_PATH, EVAL_PER_SPLIT_REGRESSION_PATH
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, mean_absolute_percentage_error
 
 class Pipeline_evaluate():
@@ -30,18 +32,87 @@ class Pipeline_evaluate():
             self.logger.error(f"[evaluate.py] Error loading data: {e}, make sure you run preprocessor.py & train.py first!")
             raise
         
-    def calculate_smape(self):
-        y_true, y_pred = np.array(self.y_test_regression), np.array(self.y_pred_regression)
+    def calculate_smape(self, y_true, y_pred):
+        y_true, y_pred = np.array(y_true), np.array(y_pred)
         denominator = (np.abs(y_true) + np.abs(y_pred)) / 2
         epsilon = 1e-10
         denominator = np.where(denominator == 0, epsilon, denominator)
         return np.mean(np.abs(y_pred - y_true) / denominator) * 100
+
+    def evaluate_per_split(self, grid_search):
+        self.logger.info("Starting Evaluation on different seeds ([1, 2, 3, 4, 5, 42, 100])...")
+        self.X_full = pd.concat([self.X_train_regression, self.X_test_regression], axis=0)
+        self.y_full = pd.concat([self.y_train_regression, self.y_test_regression]).squeeze()
+        self.idx_full = self.X_full.index
+
+        self.seeds = [1, 2, 3, 4, 5, 42, 100]
+        self.r2_list, self.mae_list, self.rmse_list, self.mape_list, self.smape_list = [], [], [], [], []
+
+        best_params_only = {
+            k.replace('regressor__regressor__', ''): v 
+            for k, v in grid_search.best_params_.items()
+        }
+
+        price_bins = pd.qcut(self.y_full, q=4, labels=False, duplicates='drop')
+
+        for seed in self.seeds:
+            X_tr, X_te, y_tr, y_te = train_test_split(
+               self.X_full, self.y_full, test_size=0.2, random_state=seed, stratify=price_bins
+            )
+
+            from sklearn.base import clone
+            model_clone = clone(grid_search.best_estimator_)
+            model_clone.fit(X_tr, y_tr)
+            y_pred = model_clone.predict(X_te)
+
+            r2 = r2_score(y_te, y_pred)
+            mae = mean_absolute_error(y_te, y_pred)
+            rmse = np.sqrt(mean_squared_error(y_te, y_pred))
+            mape = mean_absolute_percentage_error(y_te, y_pred)
+            smape = self.calculate_smape(y_te, y_pred)
+
+            self.r2_list.append(r2)
+            self.mae_list.append(mae)
+            self.rmse_list.append(rmse)
+            self.mape_list.append(mape)
+            self.smape_list.append(smape)
+            # print(f"Seed {seed:3d} | R2 = {r2:.3f} | MAE = {mae:.2f}")
+
+        def summarize_metric(values, name):
+            values = np.array(values)
+            return {
+                'Metric': name,
+                'Mean': values.mean(),
+                'Std': values.std(),
+                'Min': values.min(),
+                'Max': values.max(),
+                'Range': values.max() - values.min()
+            }
+
+        summary_df = pd.DataFrame([
+            summarize_metric(self.r2_list, 'R2'),
+            summarize_metric(self.mae_list, 'MAE'),
+            summarize_metric(self.rmse_list, 'RMSE'),
+            summarize_metric(self.mape_list, 'MAPE'),
+            summarize_metric(self.smape_list, 'SMAPE'),
+        ])
+
+        summary_df = summary_df.round(3)
+        self.logger.info(f"Summary for each split:\n{summary_df}")
+
+        return summary_df
 
     def output_json_regression(self):
         self.logger.info(f"Saving Model Evaluation Summary...")
         try:
             self.metrics_regression.to_json(EVAL_SUMMARY_REGRESSION_PATH, orient='records', indent=4)
             self.logger.info(f"Evaluation metrics saved to {EVAL_SUMMARY_REGRESSION_PATH}")
+
+            try:
+                self.summary_df.to_json(EVAL_PER_SPLIT_REGRESSION_PATH, orient = 'records', indent = 4)
+                self.logger.info(f"Evaluation metrics saved to {EVAL_PER_SPLIT_REGRESSION_PATH}")
+            except Exception as e:
+                self.logger.info(f"[SKIP per_split evaluation]: To save evaluation_per_split.json, you must run pipeline.py or train.py directly!")
 
             self.logger.info(f"Printing evaluation report into JSON format...")
             self.results_regression.to_json(EVAL_DATAFRAME_REGRESSION_PATH, orient='records', indent=4)
@@ -50,7 +121,7 @@ class Pipeline_evaluate():
             self.logger.error(f"Error saving JSON output: {e}")
             raise
         
-    def evaluate_regression(self, verbose = 0, pipeline = None):
+    def evaluate_regression(self, verbose, cv_results = None, best_index = None, pipeline = None, grid_search = None):
         self.logger.info("Evaluating model performance.")
         try:
             if pipeline is not None:
@@ -68,11 +139,15 @@ class Pipeline_evaluate():
                 else:
                     self.model_type = type(pipeline).__name__
             else:
-                self.model_type = "Unknown_Model"
+                self.model_type = "unknown_model"
+
             self.r2 = r2_score(self.y_test_regression, self.y_pred_regression)
             self.mae = mean_absolute_error(self.y_test_regression, self.y_pred_regression)
             self.mape = mean_absolute_percentage_error(self.y_test_regression, self.y_pred_regression)
-            self.smape = self.calculate_smape()
+            self.smape = self.calculate_smape(
+                self.y_test_regression,
+                self.y_pred_regression
+            )
             self.rmse = np.sqrt(mean_squared_error(self.y_test_regression, self.y_pred_regression))
 
             self.metrics_regression = pd.DataFrame({
@@ -94,6 +169,24 @@ class Pipeline_evaluate():
         self.logger.info(f"MAPE: {self.mape}")
         self.logger.info(f"SMAPE: {self.smape}")
         self.logger.info(f"RMSE: {self.rmse}")
+        self.logger.info("=" * 75)
+
+        if(best_index != None and cv_results != None):
+            self.logger.info(f"Evaluating Tuned Pipeline Regression by fold:")
+            fold_scores = [cv_results[f'split{i}_test_r2'][best_index] for i in range(15)]
+
+            self.logger.info(f"Mean R^2   : {np.mean(fold_scores):.3f}")
+            self.logger.info(f"Std R^2    : {np.std(fold_scores):.3f}")
+            self.logger.info(f"Min R^2    : {np.min(fold_scores):.3f}")
+            self.logger.info(f"Max R^2    : {np.max(fold_scores):.3f}")
+
+            self.logger.info("=" * 75)
+
+        if(grid_search != None):
+            self.logger.info(f"Evaluating Tuned Pipeline Regression random seed split:")
+            self.summary_df = self.evaluate_per_split(grid_search)
+        else:
+            self.logger.info(f"Tuned Pipeline random split evaluation can only be triggered by running pipeline.py")
         
         self.logger.info(f"Creating Dataframe Model Diagnostics (10 biggest error)...")
         try:
@@ -124,4 +217,4 @@ class Pipeline_evaluate():
         
 if __name__ == "__main__":
     pipeline_evaluate = Pipeline_evaluate()
-    pipeline_evaluate.evaluate_regression(verbose=1)
+    pipeline_evaluate.evaluate_regression(verbose=1, cv_results=None, best_index=None, pipeline = None)
